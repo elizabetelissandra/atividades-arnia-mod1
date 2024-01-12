6b18c0c4ee6ee79b8b2f4f97e45b571bfef6604c
elizabetelissandra <elizabetelissandrasilva@gmail.com>
2024-01-12 16:24:24 -0300

    create 15 files

---
+++ Aula40/Exemplo01/Mostrartabela/app.js
@@ -0,0 +1,25 @@
+const getLivros = async () => {
+    const dados = await fetch('http://localhost:3000/livros') 
+    const livros = await dados.json()
+
+    return livros
+}
+const mostrarLivros = async(listalivros) => {
+    const bodyTabela = document.querySelector('tbody')
+    listalivros.forEach(livro => {
+        bodyTabela.innerHTML += 
+        `<tr>
+        <td>${livro.id}</td>
+        <td>${livro.titulo}</td>
+        <td>${livro.autor}</td>
+        <td>${livro.anoPublicacao}</td>
+        </tr> `
+    });
+}
+
+const carregarDados = async () => {
+    const listaLivros = await getLivros()
+    console.log(listaLivros)
+    mostrarLivros(listaLivros)
+}
+carregarDados()
